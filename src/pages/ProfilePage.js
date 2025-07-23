8db8076404cfbb3ab646ef9b0128ba3e33b8e1aa
Elizxo <108903373+Elizxo@users.noreply.github.com>
2025-07-23 04:47:57 -0400

    Update ProfilePage.js

--- src/pages/ProfilePage.js
+++ src/pages/ProfilePage.js
@@ -135,12 +135,9 @@ const ProfilePage = () => {
 
                     {/* Save Button */}
                     <div className="pt-4">
-                        <button
-                            type="submit"
-                            className="w-full bg-blue-600 text-white font-bold py-3 rounded-lg hover:bg-blue-700 transition duration-300"
-                        >
-                            Save Changes
-                        </button>
+                        <button type="submit" className="save-button-pfp">
+  Save Changes
+</button>
                     </div>
                 </form>
             </div>
