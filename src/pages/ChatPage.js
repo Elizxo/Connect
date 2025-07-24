2e85b9d1f14123654d2faecd3ec463f79af0c77b
Elizxo <108903373+Elizxo@users.noreply.github.com>
2025-07-24 03:02:49 -0400

    Update ChatPage.js

--- src/pages/ChatPage.js
+++ src/pages/ChatPage.js
@@ -9,7 +9,7 @@ const ChatPg = () => {
       <Navbar />
       <main className="min-h-[calc(100vh-8rem)] p-6 bg-white">
         <h1 className="text-3xl font-bold mb-6">Chat</h1>
-        {/* Your chat content */}
+        //Chat box
       </main>
       <Footer />
     </>
