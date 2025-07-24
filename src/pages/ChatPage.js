419917e1bef47a9e0ad8bf001b80e06053421f49
Elizxo <108903373+Elizxo@users.noreply.github.com>
2025-07-24 03:23:52 -0400

    Update ChatPage.js

--- src/pages/ChatPage.js
+++ src/pages/ChatPage.js
@@ -1,19 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { ChatBubbleOvalLeftEllipsisIcon } from '@heroicons/react/24/solid';
 import { mockEvents, mockCurrentUser } from '../data/mockData';
+
 
 const ChatPg = () => {
   return (
-    <>
-      <Navbar />
-      <main className="min-h-[calc(100vh-8rem)] p-6 bg-white">
-        <h1 className="text-3xl font-bold mb-6">Chat</h1>
-        //Chat box
-      </main>
-      <Footer />
-    </>
+    <main className="min-h-[calc(100vh-8rem)] p-6 bg-white">
+      <h1 className="text-3xl font-bold mb-6">Chat Page</h1>
+      {/* chat */}
+      <p>Welcome to the chat! Start a conversation here.</p>
+    </main>
   );
 };
 
 export default ChatPg;
+
